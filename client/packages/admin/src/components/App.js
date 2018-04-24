fdf32650d798c23b1af206a2925afd2978c776ef
nli2 <a-nli2@expedia.com>
2018-04-24 15:24:43 -0700

    admin menu

--- client/packages/admin/src/components/App.js
+++ client/packages/admin/src/components/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import Menu from './Menu';
+import Home from './Home';
+import Category from './Category';
+import Products from './Products';
 
-class App extends Component {
-  render() {
-    return (
+const App = () =>
+  <div>
+    <h1>VTTC Admin</h1>
+    <hr/>
+    <div class="p16 f">
+      <Menu />
       <div>
-        abcd
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/cats' component={Category} />
+          <Route path='/products' component={Products} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
 
 export default App;
---
+++ client/packages/admin/src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () =>
+  <div class="ui vertical menu">
+    <div class="item">
+      <Link class="header cp" to="cats">Categories</Link>
+    </div>
+    <div class="item">
+      <Link class="header cp" to="products">Products</Link>
+    </div>
+  </div>
