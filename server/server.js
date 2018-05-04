122db9c3896ed9a15f029ba07c515da1debd5e72
nli2 <a-nli2@expedia.com>
2018-05-04 16:32:22 -0700

    jwt, cors, cookie

--- server/server.js
+++ server/server.js
@@ -1,55 +1,26 @@
 const express = require('express');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const api = require('./api');
 const { done, send } = require('./utils');
 
 const app = express();
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-let mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://localhost:27017/vttc';
-let mongoURLLabel = "";
-
-if (process.env.DATABASE_SERVICE_NAME) {
-  const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-  const mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-  const mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-  const mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-  const mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-  const mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-  }
-}
-else {
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-  });
-}
+if (!process.env.DATABASE_SERVICE_NAME)
+    app.use(cors);
 
 api.initdb(mongoURL);
 
 app.use(express.static('client/packages/vttc/build'));
 app.use(express.static('client/packages/admin/build'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use((req, res, next) => {
   api.initdb(mongoURL);
   next();
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
 });
 
 // get --------------------
