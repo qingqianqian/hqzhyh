86f5084b36adb84d736b53cb2319a2d422014c3c
nli2 <a-nli2@expedia.com>
2018-06-01 11:29:44 -0700

    schedule

--- client/packages/vttc/src/components/Schedule.js
+++ client/packages/vttc/src/components/Schedule.js
@@ -3,36 +3,36 @@ import { range, is, pick } from 'ramda';
 import { connect } from 'no-redux';
 import { compose } from 'recompose';
 import actions from 'utils/actions';
-import { scheduleSelector } from 'utils/selectors';
+import { tournamentSelector } from 'utils/selectors';
 import { cdurl, withLoad, withDetail, withParams, withListener, getNameById, findById, tap } from 'utils';
 import { withRouter } from 'react-router-dom';
 import { TextBox, Table } from 'utils/comps';
 import TMenu from './TMenu';
 
-const Schedule = ({ schedule, tournament, id }) =>
+const Schedule = ({ tournament, id }) =>
   <div class="p16 f">
     <TMenu id={id} />
     <div class="p32 fv">
       <h1>Schedule - {tournament.name}</h1>
       <hr/>
-      {(schedule || []).map(w =>
+      {(tournament.schedules || []).map(s =>
         <div class="pt8">
-          <div class="pt8 fs24 darkgreen">{w.date}</div>
-          <Table name="week" data={w.matches} equalWidth>
+          <div class="pt8 fs24 darkgreen">{s.date}</div>
+          <Table name="schedule" data={(s.matches || []).filter(m => m).map(m => ({ 'Table': m.id, 'Home': m.home, 'Away': m.away }))} />
+          {/* <Table name="week" data={w.matches} equalWidth>
             <td key="team1Points" hidden />  
             <td key="team2Points" hidden />  
             <td key="winner" hidden />  
             <td key="loser" hidden />  
             <td key="result" center />  
-          </Table>
+          </Table> */}
         </div>  
       )}
     </div>
   </div>
 
 export default compose(
-  connect(scheduleSelector, actions),
+  connect(tournamentSelector, actions),
   withParams,
-  withLoad('tournament', 'id'),
-  withListener('click', p => tap(p))
+  withLoad('tournament')
 )(Schedule);
